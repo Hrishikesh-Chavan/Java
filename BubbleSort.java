7a1bee16b0e7912abf0cb497c22bd4127931ca1a
Hrishikesh-Chavan <96835309+Hrishikesh-Chavan@users.noreply.github.com>
2021-12-29 21:39:45 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,4 +1,4 @@
-import java.util.*;
+
 
 public class BubbleSort {
     public static void main(String[] args) {
@@ -10,13 +10,15 @@ public class BubbleSort {
         for (int i=0;i<arr.length-1;i++){
             for(int j=0;j<arr.length-1-i;j++){
                 if(arr[j] > arr[j+1]){
+                    // Swap
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                 }
             }
         }
-
+        
+        // Print array
         for (int e: arr){
             System.out.print(e + " ");
         }
