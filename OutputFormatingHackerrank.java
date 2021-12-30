43f625926bf5410be8cdf3c9f612cc9d4cc46c85
Hrishikesh-Chavan <96835309+Hrishikesh-Chavan@users.noreply.github.com>
2021-12-30 19:34:08 +0530

    Update and rename Solution.java to OutputFormatingHackerrank.java

--- OutputFormatingHackerrank.java
+++ OutputFormatingHackerrank.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 
-public class Solution {
+public class OutputFormatingHackerrank {
     
-  // OutputFormatingHackerrank
-  
     public static void main(String[] args) {
             Scanner sc=new Scanner(System.in);
             System.out.println("================================");
