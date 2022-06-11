a5e2b6b9b2d451f08907a7aa5f1bf386fbfd9135
Hrishikesh-Chavan <96835309+Hrishikesh-Chavan@users.noreply.github.com>
2022-06-11 18:34:49 +0530

    Create EmailString.java

---
+++ EmailString.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        String email = sc.next();
+        String username = "";
+        
+        for(int i=0;i<email.length(); i++){
+            if(email.charAt(i) == '@'){
+                break;
+            }else{
+                username = username+email.charAt(i);
+            }
+        }
+        
+        System.out.print(username);
+    }
+}
